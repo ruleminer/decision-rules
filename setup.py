233f221ac72dd59da7333f353f9522a89bae229a
Bartek Piguła <bartosz.pigula@emag.lukasiewicz.gov.pl>
2024-09-25 14:31:48 +0200

    add license, add long description, readme arg and python version to setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ from setuptools import setup
 
 setup(
     name='decision_rules',
-    description='''
-    Package implementing decision rules. Includes tools for calculations of various measures and indicators,
-    as well as algorithms for filtering rulesets.
-    ''',
+    description=(
+        "Package implementing decision rules. Includes tools for calculations of various measures "
+        "and indicators, as well as algorithms for filtering rulesets."
+    ),
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     version='1.0.0',
     author='Cezary Maszczyk, Dawid Macha, Adam Grzelak',
     author_email='cezary.maszczyk@emag.lukasiewicz.gov.pl',
+    readme='README.md',
     packages=find_packages(),
     install_requires=[
         'numpy~=1.24.2',
@@ -21,4 +24,5 @@ setup(
         'imbalanced-learn~=0.10.1',
         'typeguard~=4.3.0',
     ],
+    python_requires='>=3.9',
 )
