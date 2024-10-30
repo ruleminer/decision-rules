7b8c7069880f5a0721e3fcaae8b9b106f2a9f705
adam-bialy <skrzypeknadachu@gmail.com>
2024-10-30 09:01:56 +0100

    start prediction indicator names with capital letters

--- decision_rules/survival/prediction_indicators.py
+++ decision_rules/survival/prediction_indicators.py
@@ -2,14 +2,15 @@ from typing import TypedDict
 
 import numpy as np
 import pandas as pd
+
 from decision_rules.problem import ProblemTypes
 from decision_rules.survival import SurvivalRuleSet
 
 
 class SurvivalGeneralPredictionIndicators(TypedDict):
     ibs: float
-    covered_by_prediction: int
-    not_covered_by_prediction: int
+    Covered_by_prediction: int
+    Not_covered_by_prediction: int
 
 
 class SurvivalPredictionIndicators(TypedDict):
